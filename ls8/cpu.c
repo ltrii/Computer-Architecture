b695702ff94336f9d54e61af1f948551d3fc077a
ltrii <lelandtr@gmail.com>
2019-06-12 15:32:13 -0700

    d3 work with push and pop

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #define DATA_LEN 6
+#define Sp 7
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -44,12 +45,18 @@ void cpu_load(struct cpu *cpu, char *file)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  unsigned char valueA = cpu->registers[regA];
+  unsigned char valueB = cpu->registers[regB];
 
-    // TODO: implement more ALU ops
+
+  switch (op)
+  {
+  case ALU_MUL:
+    cpu->registers[regA] = valueA * valueB;
+    break;
+  case ALU_ADD:
+    cpu->registers[regA] = valueA + valueB;
+    break;
   }
 }
 
@@ -61,48 +68,63 @@ void cpu_run(struct cpu *cpu)
   int running = 1; // True until we get a HLT instruction
   unsigned char opA;
   unsigned char opB;
-  unsigned char ir = cpu_ram_read(cpu, cpu->pc);
+  unsigned char ir;
 
   while (running) {
     // TODO
-    // 1. Get the value of the current instruction (in address PC).
+    // 1. Get the value of the current instruction (in address pc).
     // 2. Figure out how many operands this next instruction requires
     // 3. Get the appropriate value(s) of the operands following this instruction
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
-    // 6. Move the PC to the next instruction.
+    // 6. Move the pc to the next instruction.
+    ir = cpu_ram_read(cpu, cpu->pc);
 
-    if (ir >= 80)
-    {
-      opA = cpu_ram_read(cpu, cpu->pc + 1);
-      opB = cpu_ram_read(cpu, cpu->pc + 2);
-      cpu->pc += 3;
-    }
-    else if (ir >= 50)
-    {
-      opA = cpu_ram_read(cpu, cpu->pc + 1);
-      cpu->pc += 2;
-    }
+    opA = cpu_ram_read(cpu, cpu->pc + 1);
+    opB = cpu_ram_read(cpu, cpu->pc + 2);
+
     switch (ir)
     {
     case LDI:
-      cpu_ram_write(cpu, opA, opB);
+      cpu->registers[opA] = opB;
       break;
     case PRN:
-      printf("%d\n", opB);
+      printf("%d\n", cpu->registers[opA]);
       break;
     case MUL:
-      cpu->registers[opA] = (cpu->registers[0]) * (cpu->registers[1]);
-      cpu->pc += 3;
+      alu(cpu, ALU_MUL, opA, opB);
+      break;
+    case ADD:
+      alu(cpu, ALU_ADD, opA, opB);
+      break;
+    case POP:
+      cpu->registers[opA] = cpu->ram[(cpu->registers[Sp])];
+      cpu->registers[Sp]++;
+      break;
+    case PUSH:
+      cpu->registers[Sp]--;
+      cpu_ram_write(cpu, cpu->registers[Sp], cpu->registers[opA]);
+      break;
+    case CALL:
+      cpu->registers[Sp]--;
+      cpu_ram_write(cpu, cpu->registers[Sp], cpu->pc + 2);
+      cpu->pc = cpu->registers[opA];
+      break;
+    case RET:
+      cpu->pc = cpu->ram[cpu->registers[Sp]];
+      cpu->registers[Sp]++;
       break;
     case HLT:
-      exit(1);
+      running = 0;
       break;
     default:
+      printf("unexpected instruction 0x%02X at 0x%02X\n", ir, cpu->pc);
+      exit(1);
       break;
     }
 
   }
+
 }
 
 /**
@@ -110,10 +132,11 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
+  // TODO: Initialize the pc and other special registers
   cpu->pc = 0;
   memset(cpu->ram, 0, sizeof(cpu->ram));
   memset(cpu->registers, 0, sizeof(cpu->registers));
+  cpu->registers[7] = 0xf4;
 }
 
 unsigned char cpu_ram_read(struct cpu *cpu, char loc)
